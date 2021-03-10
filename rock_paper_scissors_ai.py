653e3d4725a83cdac5be47065563de11f48671ad
Chey Rose <cheyroseflammer@gmail.com>
2021-03-10 10:59:26 -0800

    ai RPS off memeory

---
+++ rock_paper_scissors_ai.py
@@ -0,0 +1,29 @@
+import random
+
+player1 = input('Enter a your choice: ')
+
+choice_list = ['rock', 'paper', 'scissors']
+
+computerPlayer = random.choice(choice_list)
+
+
+if player1 == computerPlayer:
+    print(
+        f'Its a tie! Player 1 picked {player1} and computer picked {computerPlayer}')
+elif player1 == 'rock':
+    if computerPlayer == 'scissors':
+        print('Player 1 wins, computer played scissors')
+    elif computerPlayer == 'paper':
+        print('Computer wins, computer played paper')
+elif player1 == 'paper':
+    if computerPlayer == 'rock':
+        print('Player 1 wins, computer played rock')
+    elif computerPlayer == 'scissors':
+        print('Computer wins, computer played scissors')
+elif player1 == 'scissors':
+    if computerPlayer == 'rock':
+        print('Computer wins, computer played rock')
+    elif computerPlayer == 'paper':
+        print('Player 1 wins, computer played paper')
+else:
+    print('Something went wrong')
