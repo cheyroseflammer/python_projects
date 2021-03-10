ec6e9988e356368ed9f5525d59729426feadba77
Chey Rose <cheyroseflammer@gmail.com>
2021-03-10 13:06:57 -0800

    clean up

--- rock_paper_scissors_ai.py
+++ rock_paper_scissors_ai.py
@@ -1,5 +1,9 @@
 import random
 
+print('Rock...')
+print('Paper...')
+print('Scissors...')
+
 player1 = input('Enter a your choice: ')
 
 choice_list = ['rock', 'paper', 'scissors']
@@ -9,21 +13,21 @@ computerPlayer = random.choice(choice_list)
 
 if player1 == computerPlayer:
     print(
-        f'Its a tie! Player 1 picked {player1} and computer picked {computerPlayer}')
+        f'Its a tie! Player 1 picked {player1} and computer picked {computerPlayer}!')
 elif player1 == 'rock':
     if computerPlayer == 'scissors':
-        print('Player 1 wins, computer played scissors')
+        print('Player 1 wins, computer played scissors.')
     elif computerPlayer == 'paper':
-        print('Computer wins, computer played paper')
+        print('Computer wins, computer played paper.')
 elif player1 == 'paper':
     if computerPlayer == 'rock':
-        print('Player 1 wins, computer played rock')
+        print('Player 1 wins, computer played rock.')
     elif computerPlayer == 'scissors':
-        print('Computer wins, computer played scissors')
+        print('Computer wins, computer played scissors.')
 elif player1 == 'scissors':
     if computerPlayer == 'rock':
-        print('Computer wins, computer played rock')
+        print('Computer wins, computer played rock.')
     elif computerPlayer == 'paper':
-        print('Player 1 wins, computer played paper')
+        print('Player 1 wins, computer played paper.')
 else:
-    print('Something went wrong')
+    print('Oops, something went wrong.')
