d8208b76cb44c0474509c4a73b54b5a6f6dda4f7
Chey Rose <cheyroseflammer@gmail.com>
2021-03-10 18:28:20 -0800

    emoji art challenge done

---
+++ emoji_art.py
@@ -0,0 +1,27 @@
+# Print ('\U0001f600')
+# 1-10 times on a separate line each
+# Use and for and a while loop
+# take advantage of the fact that you can multiply a string by a number and print it out
+#
+##
+###
+####
+#####
+######
+########
+#########
+##########
+
+
+# while loop
+
+num = 0
+while num < 10:
+    num += 1
+    print(num * '\U0001f600')
+
+
+# for loop
+
+for num in range(1, 11):
+    print(num * '\U0001f600')
